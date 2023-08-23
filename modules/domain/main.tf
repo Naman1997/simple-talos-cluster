6db8e46456607787ee19eaf37194e22e060780e1
Naman1997 <aroranaman17@gmail.com>
2023-08-23 06:03:33 +0530

    Use ovmf for bios and add ballooning

--- modules/domain/main.tf
+++ modules/domain/main.tf
@@ -8,20 +8,23 @@ terraform {
 }
 
 resource "proxmox_vm_qemu" "node" {
-  name                   = var.name
-  memory                 = var.memory
-  cores                  = var.vcpus
-  sockets                = var.sockets
-  onboot                 = var.autostart
-  target_node            = var.target_node
+  name        = var.name
+  memory      = var.memory
+  balloon     = var.memory
+  cores       = var.vcpus
+  sockets     = var.sockets
+  onboot      = var.autostart
+  target_node = var.target_node
+
   agent                  = 1
   clone                  = "talos-golden"
-  full_clone             = true
   boot                   = "order=scsi0"
   cpu                    = "x86-64-v2"
-  # automatic_reboot       = false
+  bios                   = "ovmf"
+  scsihw                 = "virtio-scsi-pci"
+  full_clone             = true
   define_connection_info = false
-  scsihw = "virtio-scsi-pci"
+
 
   network {
     model  = "virtio"
@@ -30,7 +33,7 @@ resource "proxmox_vm_qemu" "node" {
 }
 
 data "external" "address" {
-  depends_on = [ proxmox_vm_qemu.node ]
-  working_dir = "${path.root}"
-  program = ["bash", "scripts/ip.sh", "${lower(proxmox_vm_qemu.node.network[0].macaddr)}"]
+  depends_on  = [proxmox_vm_qemu.node]
+  working_dir = path.root
+  program     = ["bash", "scripts/ip.sh", "${lower(proxmox_vm_qemu.node.network[0].macaddr)}"]
 }
