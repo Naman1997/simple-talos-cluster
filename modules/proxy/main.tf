951ec39248bb63d6579276648c0d593c72d0336e
Naman1997 <aroranaman17@gmail.com>
2025-01-13 12:22:14 +0530

    Fix image ref, upgrade provider and update readme

--- modules/proxy/main.tf
+++ modules/proxy/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.66.3"
+      version = "0.69.1"
     }
   }
 }
@@ -47,9 +47,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type       = "iso"
   datastore_id       = "local"
   node_name          = var.TARGET_NODE
-  url                = "https://cloud-images.ubuntu.com/oracular/20241109/oracular-server-cloudimg-amd64.img"
-  checksum           = "05bbfe57d7701c685d8c65f4d34cebe947bc89e3509c4d8a2b9c77f39e91f3ca"
-  checksum_algorithm = "sha256"
+  url                = "https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
   upload_timeout     = 1000
   overwrite          = false
 }
